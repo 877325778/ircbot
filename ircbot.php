0183db0a34fc666a75fd5b4f8937beaa538209ae
Athurg <athurg@gooth.cn>
2009-12-12 22:38:47 +0800

    增加打招呼功能、规范配置变量到配置文件
    	可以指定昵称及其打招呼的内容，当该昵称登录的时候自动打招呼。
    	如果没有指定，采用默认招呼内容
    
    	将ircbot.php中的配置代码全部移动到config.php.example文件中

--- ircbot.php
+++ ircbot.php
@@ -1,24 +1,83 @@
 <?php
 //包含SmartIRC模块
-include_once("Net/SmartIRC.php");
+include_once("Net_SmartIRC/SmartIRC.php");
 include_once("weather.php");
 include_once("stat.php");
 
-date_default_timezone_set("Asia/Chongqing");
+// 用于防止本脚本重复运行，造成重复多个机器人
+$lockfile = "/tmp/ircbot.lock";
 
-#新建一个类
+// 避免多个机器人同时上线
+if(is_file($lockfile))	die("机器人已经在线了");
+
+/* 需要注册功能 */
+$funcs = array(
+		//array("监听的消息类型","匹配正则表达式字串", "回调方法名"),
+		array(SMARTIRC_TYPE_JOIN,	".*",	"sayhello"),	// 来的时候说你好！
+		array(SMARTIRC_TYPE_QUIT | SMARTIRC_TYPE_PART,	".*",	"saybye"),	//离开的时候说再见！
+		array(SMARTIRC_TYPE_CHANNEL,	"^${nick}:*\s*￥[1-9][0-9]*$",	"money"),	// 给点钱就是大爷
+		array(SMARTIRC_TYPE_CHANNEL,	"^${nick}$",			"hello"),
+		array(SMARTIRC_TYPE_CHANNEL,	"^退出$",			"quit"),
+		array(SMARTIRC_TYPE_CHANNEL,	"^${nick}:*\s*帮助",		"help"),
+		array(SMARTIRC_TYPE_CHANNEL,	"排行榜$",			"stat"),
+		array(SMARTIRC_TYPE_CHANNEL,	"^(今|明|后)天.+天气",		"weather"),
+		array(SMARTIRC_TYPE_CHANNEL,	".*",				"log")
+	);
+
+
+// 初始化对象
+$bot = &new bot();
+$irc = &new Net_SmartIRC();
+
+$irc->setUseSockets(TRUE);
+
+// 注册所需的监听功能
+foreach($funcs as $func)
+	$irc->registerActionhandler($func[0], $func[1], $bot, $func[2]);
+
+// 联接服务器登陆机器人，然后进入房间
+$irc->connect($host,$port);
+$irc->login($nick, $nick_desc);
+$irc->join($chan);
+
+// 成功登陆后建立锁定文件，用于WEB界面查询登陆状态
+// 然后开始监听
+touch($lockfile);
+$irc->listen();
+
+// 注销后删除用于查询登陆状态的锁定文件
+$irc->disconnect();
+unlink($lockfile);
+
+// 机器人类
 class bot{
-	function hello(&$irc,&$data){
-		$irc->message(SMARTIRC_TYPE_CHANNEL,
-		       		$data->channel, 
-				$data->nick.":叫什么叫，烦死了。没看见我在挖矿吗？有问题大叫“帮助”就可以了。");
-		$irc->message(SMARTIRC_TYPE_CHANNEL,
-		       		$data->channel, "/action 话说现在的活人怎么比机器人还无聊，囧orz.");
+	function sayhello(&$irc,&$data){
+		global $login_info;
 
+		$nick = strtolower($data->nick);
+		// 来人了就给他打个招呼
+		// 可以在配置文件中设置$hello_str，以对特定的人打招呼
+		if ( array_key_exists($nick, $login_info) ){
+			$irc->message(SMARTIRC_TYPE_ACTION, $data->channel, $login_info["$nick"]);
+		}else{
+			$irc->message(SMARTIRC_TYPE_CHANNEL, $data->channel, "${nick}:hi，等你老久了");
+		}
+
+		//TODO:
+		//	然后就查阅他的收件箱里是否有留言
+	}
+
+	function saybye(&$irc,&$data){
+		// 来人了就给他打个招呼
+		$irc->message(SMARTIRC_TYPE_CHANNEL, $data->channel, "我的妈啊，".$data->nick."终于走了");
+
+		//TODO:
+		//	然后就查阅他的收件箱里是否有留言
 	}
 
 	// 接收指令退出
 	function quit(&$irc, &$data) {
+
 		if($data->nick=="athurg"){
 			$irc->quit("Mumm call me to have meal...");
 		}
@@ -42,7 +101,7 @@ class bot{
 			"有想法？找Athurg <athurg#gooth.cn>！"
 		);
 		foreach($help as $msg){
-			$irc->message(SMARTIRC_TYPE_CHANNEL, $data->channel, $msg);
+			$irc->message(SMARTIRC_TYPE_QUERY, $data->nick, $msg);
 		}
 	}
 
@@ -57,11 +116,7 @@ class bot{
 					如您有任何问题，烦请联系Athurg<athurg#gooth.cn>\n"
 				);
 		}
-		file_put_contents(
-				$filename, 
-				date("[Y-m-d H:i:s]<").$data->nick.">:".$data->message."\n", 
-				FILE_APPEND
-			);
+		file_put_contents($filename, date("[Y-m-d H:i:s]<").$data->nick.">:".$data->message."\n", FILE_APPEND);
 
 		// 更新统计信息
 		update_stat($data->nick, $data->message);
@@ -84,55 +139,4 @@ class bot{
 				weather_check($rst[2], $rst[1]));
 	}
 }
-// 机器人登陆位置设置
-$host = "irc.oftc.net";
-$port = 6667;
-$nick = "at";
-$nick_desc = "Athurg's Bot";
-$chan = array(
-		"#arch-cn",
-		"#steamedfish",
-	);
-
-$lockfile = "/tmp/ircbot.lock";
-
-// 避免多个机器人同时上线
-if(is_file($lockfile))	die("机器人已经在线了");
-
-/* 注册功能 */
-$funcs = array(
-		//array("匹配正则表达式", "回调方法名"),
-		array("^${nick}:*\s*￥[1-9][0-9]*$", "money"),
-		array("^${nick}$","hello"),
-		array("^你妈喊你回去吃饭$","quit"),
-		array("^${nick}:*\s*帮助","help"),
-		array("排行榜$","stat"),
-		array("^(今|明|后)天.+天气","weather"),
-		array(".*","log")
-	);
-
-
-// 初始化对象
-$bot = &new bot();
-$irc = &new Net_SmartIRC();
-$irc->setUseSockets(TRUE);
-
-// 注册所需的监听功能
-foreach($funcs as $func){
-	$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL,$func[0], $bot, $func[1]);
-}
-
-// 联接服务器登陆机器人，然后进入房间
-$irc->connect($host,$port);
-$irc->login($nick, $nick_desc);
-$irc->join($chan);
-
-// 成功登陆后建立锁定文件，用于WEB界面查询登陆状态
-// 然后开始监听
-touch($lockfile);
-$irc->listen();
-
-// 注销后删除用于查询登陆状态的锁定文件
-$irc->disconnect();
-unlink($lockfile);
 ?>
