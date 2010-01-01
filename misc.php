61272c24e99eba217334f771f60d165ac959833c
athurg <athurg@myhost.localdomain>
2010-01-01 10:23:10 +0800

    查字典功能增加特殊字典
    misc.php
    	增加特殊字典，比如聊天室的人员名字的字典等。

--- misc.php
+++ misc.php
@@ -16,17 +16,32 @@
  */
 
 function check_dict($word){
+	$word=trim($word);
+	$custom_dict=array(
+		'muxueqz'=>'那个卖服务器的！',
+		'athurg'=>'找我老大有事儿？',
+		'steamedfish'=>'传说中用油炸的蒸鱼……',
+		'ghostm55'=>'鬼门关左护卫！',
+		'zfish'=>'机器人！！',
+		'at'=>'万能的神，伟大的真主。',
+		'huntxu'=>'机器人附身了！',
+		//''=>'',
+	);
+	if(array_key_exists(strtolower($word),$custom_dict)){
+		$special_mean = $custom_dict[strtolower($word)];
+	}
 	$uri="http://dict.youdao.com/search?q=${word}";
 	$content=file_get_contents($uri);
 	$content=strstr($content, '<td valign');
 	if (!$content){
-		return "抱歉，没有查到{$word}";
+		$content= $special_mean ? $special_mean : "抱歉，没有查到{$word}。";
 	}else{
 		$content=strstr($content, '</table>', true);
 		// 这里采用正则表达式效率稍高，且代码较优雅
 		$content=preg_replace('/<([^>]+?)>/s', '',$content);
 		$content=preg_replace('/\s/s', '', $content);
 		$content=str_replace('&nbsp;', '', $content);
+		$content.="      也可能意指".$special_mean;
 	}
 	return $content;
 }
